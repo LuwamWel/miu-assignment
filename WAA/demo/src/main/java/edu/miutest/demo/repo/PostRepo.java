f1a61bede5bffa43baac1384f766e9e1a4382f3a
LuwamWel <luwamgwm@gmail.com>
2024-05-29 23:36:01 -0500

    lab1 initial commit

---
+++ WAA/demo/src/main/java/edu/miutest/demo/repo/PostRepo.java
@@ -0,0 +1,14 @@
+package edu.miutest.demo.repo;
+
+import edu.miutest.demo.entity.Post;
+
+import java.util.List;
+
+public interface PostRepo {
+
+    List<Post> findAll();
+    Post findById(long id);
+    void save(Post p);
+    void delete(long id);
+    void update(long id, Post p);
+}
